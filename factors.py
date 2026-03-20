64ae329ea1f356b7be597a2ba20b89d15194995b
etaiozary-lab <etaiozary@gmail.com>
2026-03-20 20:58:35 +0200

    Create factors.py

---
+++ factors.py
@@ -0,0 +1,14 @@
+def factors(x):
+    f_list = []
+
+    for i in range(1, x + 1):
+        if x % i == 0:
+            f_list.append(i)
+
+    return f_list
+
+# Check your code
+print(factors(6))
+print(factors(17))
+print(factors(24))
+print(factors(48))
